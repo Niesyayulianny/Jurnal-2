53b9c90e3c0e0c6cd974097f4b95d1dcada99c1d
Niesyayulianny <niesyayulianny@student.telkomuniversity.ac.id>
2020-09-18 10:40:54 +0700

    niesya.java

---
+++ Niesyul/niesya.java
@@ -0,0 +1,8 @@
+public class niesya {
+
+	public static void main(String[] args){
+		System.out.println("Nama: Niesya Yulianny");
+		System.out.println("Nim: 6706201030");
+		System.out.println("Kelas: 44-03");
+	}
+}
